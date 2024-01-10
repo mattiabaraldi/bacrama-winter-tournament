bb6b84b3eae5863b51b4757aed6d55407548a255
mattiabaraldi <m.mattiabaraldi@gmail.com>
2024-01-10 20:24:52 +0100

    socket working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,57 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import {io} from 'socket.io-client';
+
+let socket;
 
 const App = () => {
 
-  const [database, setDatabase] = useState();
-
-  useEffect(() => {
-    fetch('/api/getDatabase',
-    {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json",
-      }
-    })
-    .then(response => {
-      response.json()
-      .then(data => {
-        console.log(data);
-        setDatabase(JSON.stringify(data));
-      })
-    })
-  }, []);
+  const [fighters, setFighters] = useState({});
+  const [socket, setSocket] = useState(() => {
+    const socket = io();
+    socket.on('disconnect', () => console.log('disconnect'));
+    socket.on('connect_error', () => {
+      setTimeout(() => socket.connect(), 5000);
+    });
+    socket.on('serveFighters', data => setFighters(data));
+    socket.on('connect', () => socket.emit('getFighters'));
+    return socket;
+  });
   
+  const addFighter = e => {
+    e.preventDefault();
+    socket?.emit('addFighter', {
+      name: e.target.elements.name.value,
+      level: e.target.elements.level.value
+    });
+  }
+
   return (
-    <>{database}</>
+    <>
+      <form onSubmit={addFighter}>
+        <div style={{
+          height: '200px',
+          width: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+        }}>
+          <div>
+            <p>Nome:</p>
+            <input name="name" />
+          </div>
+          <div>
+            <p>Livello:</p>
+            <input type='number' name="level" />
+          </div>
+          <div style={{paddingTop: '30px'}}>
+            <button type="submit">Aggiungi</button>
+          </div>
+        </div>
+        {Object.entries(fighters).map(([key, value]) => {
+          return <p>{`${key}: ${value}`}</p>;
+        })}
+      </form>
+      
+    </>
   );
 }
 
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { createServer } from 'node:http';
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 3000;
+const server = createServer(app);
+const io = new Server(server);
+let ioSocket = null;
+
+const fighters = {};
 
 app.use(express.json());
 app.use(cors());
@@ -15,16 +22,22 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(__dirname + '/public/'));
 
-app.get('/api/getDatabase', (req, res) => {
-  res.send(readFileSync('./database.json', 'utf8'));
-});
-
 app.on('clientError', (err, socket) => {
   if (err.code === 'ECONNRESET' || !socket.writable) socket.end('HTTP/2 400 Bad Request\n');
   console.log('client error\n', err);
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  socket.on('getFighters', data => {
+    io.emit('serveFighters', fighters);
+  });
+  socket.on('addFighter', data => {
+    fighters[data.name] = data.level;
+    io.emit('serveFighters', fighters);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
