dc0532b2c27b7914907292ccde1e8b0dbf60b1ed
mattiabaraldi <m.mattiabaraldi@gmail.com>
2024-01-25 13:03:42 +0100

    forgot ip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
 
   const [bacchiatori, setBacchiatori] = useState({});
   const [socket, setSocket] = useState(() => {
-    //const socket = io('/', {transports: ['websocket']});
-    const socket = io('http://localhost:3000', {transports: ['websocket']});
+    const socket = io('/', {transports: ['websocket']});
+    //const socket = io('http://localhost:3000', {transports: ['websocket']});
     socket.on('disconnect', () => console.log('disconnect'));
     socket.on('connect_error', () => {
       setTimeout(() => socket.connect(), 5000);
